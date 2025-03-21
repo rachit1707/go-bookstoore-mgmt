2a9cbe928ccf609fd2e905a019d431f140b345ce
rachit1707 <rachitsharma2020@gmailc.om>
2025-03-21 10:40:40 +0530

    BookStore Go Commit

---
+++ pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:XXXXX@tcp(0.0.0.0:50487)/bookstore?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
---
+++ pkg/models/book.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/rachit1707/go-bookstoore-mgmt/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name       string `gorm:"" json:"name"`
+	Author     string `json:"author"`
+	Publicaton string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
+}
